2e21fc60ec5e274ce5d29971c525b6f3766d8587
Bien Nguyen <bien.g.nguyen@gmail.com>
2017-01-18 12:02:42 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,7 @@ class ProfilesController < ApplicationController
   # GET to /users/:user_id/profiles/new
   def new
     # Render blank profile details form
+    @profile = Profile.new
   end
 
 end
